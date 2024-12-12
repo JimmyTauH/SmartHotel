072c7061334b2fc1a399a992e8c84d85eee1efab
贺镇涛 <1427113362@outlook.com>
2024-12-12 20:09:32 +0800

    FixTextBug

--- HotelMSFront/src/components/Footer.vue
+++ HotelMSFront/src/components/Footer.vue
@@ -1,7 +1,6 @@
 <template>
 <div style="font-size: 16px; padding:20px 0; text-align: center; line-height: 30px;color: #666666">
   <div>HotelMS智能酒店平台</div>
-  <div>HotelMS智能酒店平台是高含金量酒店预订。为全国大学生提供各类高含金量酒店信息聚合、报名和团队招募服务</div>
   <div>武汉大学大型应用软件设计小组NO.2</div>
 </div>
 </template>
